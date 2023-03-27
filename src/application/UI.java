7e278e42bc5a64514cb1549074aa7b49f995394a
João Cunha <jocunha204@gmail.com>
2023-03-27 11:12:46 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,10 +1,7 @@
 package application;
 
 import boardgame.BoardException;
-import chess.ChessException;
-import chess.ChessPiece;
-import chess.ChessPosition;
-import chess.Color;
+import chess.*;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -47,6 +44,13 @@ public class UI {
         } catch (RuntimeException e) {
             throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
         }
+    }
+
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
